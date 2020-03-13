2d018cc12f08e3730ae29821b5e7210e254ff380
rishi garg <rishigarg@rishis-MacBook-Pro.local>
2020-03-13 21:12:39 +0530

    comm

---
+++ nodejs2/node-server/server.js
@@ -0,0 +1,28 @@
+const express=require('express')
+const server=express()
+
+server.use(function(req,res,next){
+    console.log("middelware function")
+    next()
+})
+
+
+server.get('/',function(req,res,next){
+    console.log("main function")
+    res.send('Hello World !!')
+})
+
+server.get("/hello/:name", (req,res,next)=>{
+    name=req.params.name
+    surname=req.query.surname
+    console.log("hello function")
+    res.send("hello "+name+" "+surname)
+})
+
+
+// A 404 ERROR Middleware
+server.use((req,res)=>{
+    res.send("<h2>404 Not Found !!</h2>")
+})
+
+server.listen(4040)
